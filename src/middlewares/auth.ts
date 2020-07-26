6b472a17d04abe46ac7dfab73a4ac2847a411a29
Henry Otighe <oti4me@Henry-Otighe-MacBook-Pro.local>
2020-07-26 21:09:51 +0100

    add missing function doc

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -3,6 +3,14 @@ import { decode } from '../helpers/jwt';
 import { unauthorised } from '../helpers/response';
 import { User } from '../models/index';
 
+/**
+ * Protects routes that requires authentitcation
+ *
+ * @param {Request} req
+ * @param {Response} res
+ * @param {() => {}} next
+ * @returns
+ */
 export const auth = async (req: Request, res: Response, next: () => {}) => {
   const token = req.headers.authorization;
 
@@ -16,6 +24,12 @@ export const auth = async (req: Request, res: Response, next: () => {}) => {
   next();
 };
 
+/**
+ * Verify user for provided token
+ *
+ * @param {number} userId
+ * @returns
+ */
 export const verifyUser = (userId: number) => {
   const user = User.findByPk(userId);
 
